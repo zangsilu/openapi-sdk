9a39bfd70d31e680c8f6d8c8f604142a34f5452a
wangaibo <wangaibo@boqii.net>
2017-04-21 11:24:54 +0800

    增加秒杀

---
+++ src/Promotion/SecKill.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Vendor_Promotion_SecKill
+ *
+ * PHP version 5.2+
+ *
+ * @category  Promotion
+ * @package   Vendor\Promotion
+ * @author    wangaibo <wangaibo@boqii.net>
+ * @copyright 2016-2019 guangcheng Co. All Rights Reserved.
+ * @license   http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @version   GIT:<git_id>
+ * @link      http://shop.openapi.boqii.com
+ */
+class Vendor_Promotion_SecKill extends Vendor_Api
+{
+    /**
+     * getSecKillGoodsList
+     *
+     * @param mixed $criteria
+     *
+     * @access public
+     *
+     * @return mixed
+     */
+    public function getSecKillGoodsList($criteria)
+    {
+        $url = 'promotion/seckill?act=getSecKillGoodsList';
+
+        return $this->client->get($url, $criteria)->toArray();
+    }
+}
