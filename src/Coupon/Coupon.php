e0faec39c377c3cd8b783b8949fd0c6ceb5e9288
liugj <liugj@boqii.com>
2017-06-27 03:03:03 +0000

    修改注释

--- src/Coupon/Coupon.php
+++ src/Coupon/Coupon.php
@@ -4,8 +4,8 @@
  *
  * PHP version 5.2+
  *
- * @category  Goods
- * @package   Vendor\Goods
+ * @category  Coupon
+ * @package   Vendor\Coupon
  * @author    Guojing Liu <liugj@boqii.com>
  * @copyright 2016-2019 guangcheng Co. All Rights Reserved.
  * @license   http://opensource.org/licenses/gpl-2.0.php GNU General Public License
