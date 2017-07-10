eacae0bce42c2c55b16a5ddc1cd28003c115e984
zuozuoba <zhangzb@boqii.com>
2017-07-10 16:43:34 +0800

    预售, 添加预售详情接口

---
+++ src/Promotion/PreSale.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Vendor_Promotion_FullCoupon
+ *
+ * PHP version 5.2+
+ *
+ * @category  Promotion
+ * @package   Vendor\Promotion
+ * @author    zhangzhibin
+ * @copyright 2016-2019 guangcheng Co. All Rights Reserved.
+ * @license   http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @version   GIT:<git_id>
+ * @link      http://shop.openapi.boqii.com
+ */
+class Vendor_Promotion_PreSale extends Vendor_Api
+{
+    /**
+     * pushOrder
+     *
+     * @param mixed $criteria
+     *
+     * @access public
+     *
+     * @return mixed
+     */
+    public function PreSaleDetail($criteria)
+    {
+        $criteria['Act'] = 'PreSaleDetail';
+
+        return $this->client->post('?PreSaleDetail', $criteria);
+    }
+}
