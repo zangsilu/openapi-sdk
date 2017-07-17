a55f1aea433956d431e18fb9e221497d8a1f9e37
wangyongtao <wangtom365@qq.com>
2017-07-17 18:42:54 +0800

    预售商品初始化数据

--- src/Promotion/PreSale.php
+++ src/Promotion/PreSale.php
@@ -33,7 +33,7 @@ class Vendor_Promotion_PreSale extends Vendor_Api
 
     /**
      * 通过活动ID获取预售商品列表 (供预售活动模板使用)
-     * 
+     *
      * @param $criteria
      * @return mixed
      */
@@ -43,4 +43,18 @@ class Vendor_Promotion_PreSale extends Vendor_Api
 
         return $this->client->post('?GetPreSaleGoodsListByActivityIds', $criteria);
     }
+
+    /**
+     * 获取预售商品初始化数据（格式同普通商品购物车初始化）
+     *
+     * @param $criteria
+     * @return mixed
+     */
+    public function GetInitPreSaleProductList($criteria)
+    {
+        $criteria['Act'] = 'GetInitPreSaleProductList';
+
+        return $this->client->post('?GetInitPreSaleProductList', $criteria);
+    }
+
 }
