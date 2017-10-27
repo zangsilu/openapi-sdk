383ddef132549a94fe7118b4815fb2ff5cc99d4f
liugj <liugj@boqii.com>
2017-10-27 06:24:26 +0000

    增加收货人地址管理

--- src/User/Address.php
+++ src/User/Address.php
@@ -16,5 +16,100 @@
  */
 class Vendor_User_Address extends Vendor_Api
 {
- 
+    /**
+     * 详情
+     *
+     * @param mixed $criteria
+     *
+     * @access public
+     *
+     *
+     * @return mixed
+     */
+    public function getUserAddressDetail($criteria)
+    {
+        $criteria['Act'] = 'GetUserAddressDetail';
+
+        return $this->client->post('', $criteria);
+    }
+    /**
+     * 添加
+     *
+     * @param mixed $criteria
+     *
+     * @access public
+     *
+     *
+     * @return mixed
+     */
+    public function addUserAddress($criteria)
+    {
+        $criteria['Act'] = 'AddUserAddress';
+
+        return $this->client->post('', $criteria);
+    }
+    /**
+     * 设置默认
+     *
+     * @param mixed $criteria
+     *
+     * @access public
+     *
+     *
+     * @return mixed
+     */
+    public function setDefaultAddress($criteria)
+    {
+        $criteria['Act'] = 'SetDefaultAddress';
+
+        return $this->client->post('', $criteria);
+    }
+    /**
+     * 修改
+     *
+     * @param mixed $criteria
+     *
+     * @access public
+     *
+     *
+     * @return mixed
+     */
+    public function updateUserAddress($criteria)
+    {
+        $criteria['Act'] = 'UpdateUserAddress';
+
+        return $this->client->post('', $criteria);
+    }
+    /**
+     * 删除
+     *
+     * @param mixed $criteria
+     *
+     * @access public
+     *
+     *
+     * @return mixed
+     */
+    public function deleteAddress($criteria)
+    {
+        $criteria['Act'] = 'DeleteAddress';
+
+        return $this->client->post('', $criteria);
+    }
+    /**
+     * 列表
+     *
+     * @param mixed $criteria
+     *
+     * @access public
+     *
+     *
+     * @return mixed
+     */
+    public function getUserAddressList($criteria)
+    {
+        $criteria['Act'] = 'GetUserAddressList';
+
+        return $this->client->post('', $criteria);
+    }
 }
