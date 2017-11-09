6e1367b0b3db3a152eb6c29398d03b4bf7b518ab
liugj <liugj@boqii.com>
2017-11-09 10:12:54 +0000

    增加运费计算

---
+++ src/Shopping/Freight.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Vendor_Shopping_Freight
+ *
+ * PHP version 5.2+
+ *
+ * @category  Freight
+ * @package   Vendor\Shopping
+ * @author    Guojing Liu <liugj@boqii.com>
+ * @copyright 2016-2019 guangcheng Co. All Rights Reserved.
+ * @license   http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @version   GIT:<git_id>
+ * @link      http://shop.openapi.boqii.com
+ * @see       http://code.boqii.com/management/doc/blob/master/%E5%95%86%E5%9F%8EAPI%E6%8E%A5%E5%8F%A3.md#93-%E8%8E%B7%E5%8F%96%E8%B4%AD%E7%89%A9%E8%BD%A6%E5%95%86%E5%93%81%E4%BF%A1%E6%81%AFgetshoppingcartdetailv2
+ */
+class Vendor_Shopping_Freight extends Vendor_Api
+{
+    /**
+     * 计算运费 GetFreightageCosts
+     *
+     * @param mixed $criteria
+     *
+     * @access public
+     *
+     * @return mixed
+     */
+    public function getFreightageCosts($criteria)
+    {
+        $criteria['Act'] = 'GetFreightageCosts';
+
+        return $this->client->post('', $criteria);
+    }
+}
