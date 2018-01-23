bdd81d0028f6ef9cfcebfe99495d5cb78ed5f481
wangaibo <wangaibo@boqii.net>
2018-01-23 17:40:10 +0800

    红包

---
+++ src/RedPacket/RedPacket.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Vendor_RedPacket_RedPacket
+ *
+ * PHP version 5.2+
+ *
+ * @category  RedPacket
+ * @package   Vendor\RedPacket
+ * @author    wangaibo <wangaibo@boqii.net>
+ * @copyright 2016-2019 guangcheng Co. All Rights Reserved.
+ * @license   http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @version   GIT:<git_id>
+ * @link      http://shop.openapi.boqii.com
+ */
+class Vendor_RedPacket_RedPacket extends Vendor_Api
+{
+    /**
+     * GetMyRedPacketList 
+     * 
+     * @param array $criteria 
+     * 
+     * @access public
+     * 
+     * @return mixed
+     */
+    public function getMyRedPacketList($criteria)
+    {
+        $criteria['Act'] = 'GetMyRedPacketList';
+
+        return $this->client->post('', $criteria)->toArray();
+    }
+}
